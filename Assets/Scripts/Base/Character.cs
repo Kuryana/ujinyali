67581a14d8547c991c8e0b94e1c472f612867a66
krlan2789 <ahlipdkno.2@gmail.com>
2020-08-29 20:18:12 +0700

    Add Project

---
+++ Assets/Scripts/Base/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+  protected string name;
+  protected float speed;
+
+  public static Vector3 Move()
+  {
+    return Vector3.zero;
+  }
+
+  public static void Berbicara()
+  {
+
+  }
+
+  protected static void UseLighting()
+  {
+
+  }
+}
